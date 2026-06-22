408c31022ca1acafc74b50134f7d689fc502e6b7
darkhorse0204 <darkhorse022005@gmail.com>
2026-06-22 22:23:22 +0530

    chore: add gitignore and HTML entry point

--- src/lib/src/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const SUPABASE_URL = 'https://acyftmjnsygxculgwbpo.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFjeWZ0bWpuc3lneGN1bGd3YnBvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5NzgwMjYsImV4cCI6MjA5NzU1NDAyNn0.349AvCag73RWfsUhqKyYnm_JjQBpryo_OnHND-RLxco'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
